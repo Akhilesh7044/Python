3c4946d0f4b2f45420829bee39d3b058cc9eb875
Akhilesh Patil <93441998+Akhilesh7044@users.noreply.github.com>
2023-03-08 12:32:25 +0530

    Update Project_Band_name_generator.py

--- Project_Band_name_generator.py
+++ Project_Band_name_generator.py
@@ -1,3 +1,6 @@
+
+
+
 print("Welcome to the Band Name Generator.")
 city=input("Enter city that you grew up in:\n")
 
@@ -5,4 +8,9 @@ city=input("Enter city that you grew up in:\n")
 pet_name=input("Enter your pet name:\n")
 #Combine the name of their city and pet and show them their band name
 print("Band Name: "+city+" "+pet_name+"\n")
+
+
+
+
+
 
