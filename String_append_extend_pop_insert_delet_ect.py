6fa50eab77f5411274d3cbc3cfc56ad5f7bd133e
Akhilesh Patil <93441998+Akhilesh7044@users.noreply.github.com>
2023-03-17 23:38:13 +0530

    Create String_append_extend_pop_insert_delet_ect.py

---
+++ String_append_extend_pop_insert_delet_ect.py
@@ -0,0 +1,21 @@
+
+
+
+name1=["Mumbai","Delhi","Pune","Benglore","Kolkata"]
+name=["New york","Paris","London","Tokiyo"]
+name1.append("Kolhapur") # will insert kolhapur in last of the list
+name1.extend(name) # will add to string together
+print(name1)
+name1.insert(3,"Chennai") # Will add Chennai in 3 rd index means 4th position in list 
+print(name1)
+name1.remove("Benglore") # Will remove the first element who's value is equal to Benglore
+print(name1)
+name1.pop()# pop item in list and return it also means
+p=name1.pop()
+print(p)
+print(name1)
+name.clear()#will remove all elements from the list
+print(name)
+
+
+
